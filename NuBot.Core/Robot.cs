12ead5c9b7b4ec6d6e1d2b47234f755a477a1bff
anurse <andrew@andrewnurse.net>
2013-01-30 15:08:39 -0800

    Initial code

---
+++ NuBot.Core/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuBot.Core
+{
+    public class Robot
+    {
+        public IList<IPart> Parts { get; private set; }
+
+        public Robot()
+        {
+            Parts = new List<IPart>();
+        }
+    }
+}
